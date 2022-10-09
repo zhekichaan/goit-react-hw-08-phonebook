cd7766ebc80eb1ae82e65b85418067c52bc2a669
zhekichaan <eugene.chernitskiy@gmail.com>
2022-10-08 22:17:06 -0400

    1

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,14 +1,8 @@
 import { logIn } from 'redux/auth/operations';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { useDispatch } from 'react-redux';
-import { register } from 'redux/auth/operations';
-// import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-// import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
